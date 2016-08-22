3ebf9fe6dbd71f608a29210f84baf1c685bf9615
MacbookPro <macbookpro@MacbookPros-MacBook-Pro.local>
2016-08-22 19:36:38 +1200

    Initial Commit

---
+++ server.rb
@@ -0,0 +1,48 @@
+require "socket"
+
+class Server
+
+  def initialize ip, port
+    @server  = TCPServer.open(ip,port)
+    @connections = Hash.new
+    @rooms = Hash.new
+    @clients = Hash.new
+    @connections[:server] = @server
+    @connections[:rooms] = @rooms
+    @connections[:clients] = @clients
+    run
+
+  end
+
+  def run
+    loop {
+      Thread.start(@server.accept) do |client|
+        client.puts "Welcome to SafeChat - Please type your name to start chatting - "
+        nick_name = client.gets.chomp.to_sym
+        @connections[:clients].each do |other_name, other_client|
+          if nick_name == other_name || client == other_client
+            client.puts "This username already exists"
+            Thread.kill self
+          end
+        end
+        puts "#{nick_name} #{client}"
+        @connections[:clients][nick_name] = client
+        client.puts "Connection established, Thank you for joining this safe chat"
+        listen_user_messages(nick_name, client)
+      end
+    }.join
+  end
+
+  def listen_user_messages(username, client)
+    loop {
+      msg = client.gets.chomp
+      @connections[:clients].each do |other_name, other_client|
+        unless other_name == username
+          other_client.puts "#{username.to_s}: #{msg}"
+        end
+      end
+    }
+  end
+end
+
+server = Server.new("localhost", 42169)
